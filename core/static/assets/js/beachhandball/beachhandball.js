d75b7dc1deb4a2114070685277ea9cd56d950836
Tim Kunz <tim.kunz@schmoll-maschinen.de>
2021-03-18 22:43:37 +0100

    basic crud for court

--- core/static/assets/js/beachhandball/beachhandball.js
+++ core/static/assets/js/beachhandball/beachhandball.js
@@ -3,13 +3,15 @@ console.log("bh document ready");
     $("#bhAddCourtForm_OK").click(function(){ bh.addCourt_Click(); });
 });
 
+bh_gui = {
+    
+}
+
 bh = {
     misc: {
-      navbar_menu_visible: 0,
-      active_collapse: true,
-      disabled_collapse_init: 0,
+      test_mode_active: 1,
     },
-    
+
     addCourt_Click: function(){
         console.log("addCourt_Click");
         
@@ -25,32 +27,18 @@ bh = {
         {
             console.log("Close Modal");
 
-            var response = tournament_service.postCourt(data);
-
-            // ONLY FOR TESTING
-            if(data.number == 777)
-            {
-                response.success = 'false';
-            }
+            var response = tournament_service.createCourt(data);
             
             // Hide modal frame
             var modal = document.getElementById("bhAddCourtModal");
             $(modal).modal('toggle');
 
             // evaluate server request and set notification
-            var message = "";
-            var message_type = 'success';
             if(response.success == "true")
             {
-                message = `Created court ${data.name}`;
-                bh.addCourtHTML(response.court);
+                bh.addCourtHTML(response.data);
             }
-            else
-            {
-                message = "Creating court failed! Try again...";
-                message_type = 'danger';
-            }
-            md.showNotification('bottom','center', message, message_type);
+            md.showNotification('bottom','center', response.message, response.message_type);
         }
         else {
             var validator = $(form).validate();
@@ -62,57 +50,47 @@ bh = {
         var templateCourt = $("#templateCourt").clone();
         $(templateCourt).attr("id", 'court_' + courtData.id);
         $(templateCourt).attr('obj_id', courtData.id);
+        $(templateCourt).attr('data-obj', JSON.stringify(courtData));
         $(templateCourt).removeAttr('hidden');
-        
+        $(templateCourt).find("#templateCourt_btnUpdate").click(function(){ bh.updateCourt_Click($(this)); });
+        $(templateCourt).find("#templateCourt_btnRemove").click(function(){ bh.removeCourt_Click($(this)); });
         $(templateCourt).find(".card-category").text(courtData.number);
         $(templateCourt).find(".card-title").text(courtData.name);
         $("#court_table").append(templateCourt);
         return;
-        //console.log(JSON.stringify( tournament_service.getCourts()));
-        console.log("addCourtHTML()" + courtData);
-    var title = courtData.name;
-    var category = courtData.number;
-    var court_table = document.getElementById("court_table");
-    var d = document.createElement('div');
-    var card = document.createElement('div');
-    var card_header = document.createElement('div');
-    var card_icon = document.createElement('div');
-    var card_footer = document.createElement('div');
-    $(d).addClass("col-md-3");   
-    $(card).addClass("card card-stats");
+    },
 
-    //Header
-    $(card_header).addClass("card-header card-header- card-header-icon");
-    $(card_icon).addClass("card-icon").html('<i class="material-icons">view_array</i>');
-    
-    var card_cat = document.createElement('h4');
-    $(card_cat).addClass("card-category");
-    card_cat.innerHTML = category;
-    var card_title = document.createElement('h3');
-    $(card_title).addClass("card-title");
-    card_title.innerHTML = title;
+    updateCourt_Click: function(node){
+        console.log("updateCourt_Click");
+        var topnode = node.parent().closest('.topnode');
+        var court = JSON.parse(topnode.attr('data-obj'));
+        console.log(topnode);
+        console.log(court);
+    },
 
-    card_header.appendChild(card_icon);
-    card_header.appendChild(card_cat);
-    card_header.appendChild(card_title);
-
-    // footer
-    $(card_footer).addClass("card-footer");
-    var card_footer_stats = document.createElement('div');
-    $(card_footer_stats).addClass("stats").html('<i class="material-icons">groups</i><a href="#">1500 spectators...</a>');
-    card_footer.appendChild(card_footer_stats);
-
-    // add child
-
-    card.appendChild(card_header);
-    card.appendChild(card_footer);
-    d.appendChild(card);
-    court_table.appendChild(d);
-    //var newdiv = document.createElement("div");
-    //court_table.append(document.parseHTML("<div id='dynamic'>HUHU</div>"));
-
-    tournament_service.getCourts().forEach(element => {console.log(element.name)});
-    md.showNotification('bottom','center', "Court created", 'success');
-       
+    removeCourt_Click: function(node){
+        console.log("removeCourt_Click");
+        var topnode = node.parent().closest('.topnode');
+        var court = JSON.parse(topnode.attr('data-obj'));
+        swal({
+            title: 'Are you sure delete ' + court.name + '?',
+            text: 'Court ' + court.name + ' will be deleted!',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, keep it',
+            confirmButtonClass: "btn btn-success",
+            cancelButtonClass: "btn btn-danger",
+            buttonsStyling: false
+          }).then(function(dismiss) {
+            if (dismiss.dismiss !== 'cancel')
+            {
+                var response = tournament_service.removeCourt(court.id);
+                if(response.success == "true")
+                {
+                    topnode.remove();
+                }
+            }
+          })
     },
 }
--- core/static/assets/js/beachhandball/services/tournament_service.js
+++ core/static/assets/js/beachhandball/services/tournament_service.js
@@ -24,5 +24,105 @@ tournament_service =
         };
         return result;
         
-    }
+    },
+
+    misc: {
+        test_mode_active: 1,
+      },
+  
+      mockData: {
+          courts: [ { id: 1, created_at: "2020.10.10 10:11:12", name: "Center Court", number: 1},
+          { id: 2, created_at: "2020.10.10 10:11:12", name: "Court", number: 2},
+          { id: 3, created_at: "2020.10.10 10:11:12", name: "Court3", number: 3}]
+      },
+
+      saveMockData: function() {
+        localStorage['mockData'] = JSON.stringify(tournament_service.mockData);
+      },
+
+      loadMockData: function() {
+        tournament_service.mockData = JSON.parse(localStorage['mockData']);
+      },
+  
+      getCourts: function() {
+          if(tournament_service.misc.test_mode_active == 0)
+          {
+              return tournament_service.mockData.courts;
+          }
+          else
+          {   
+            tournament_service.loadMockData();
+              var response = { message: "Get all courts" , data: JSON.parse(JSON.stringify(tournament_service.mockData.courts)), message_type: "success", success: "true"}
+              return response;
+          }
+      },
+      
+      getCourt: function(id) {
+          if(tournament_service.misc.test_mode_active == 0)
+          {
+              return tournament_service.mockData.courts.filter(court => { return court.id === id});
+          }
+          else
+          {   
+            tournament_service.loadMockData();
+            var response = "";
+            var court = tournament_service.mockData.courts.filter(court => { return court.id === id})
+            if(court.length == 1)
+                response = { message: "Get court " + id , data: JSON.parse(JSON.stringify(court[0])), message_type: "success", success: "true"}
+            else
+                response = { message: "Get court " + id , data: JSON.parse(JSON.stringify(court[0])), message_type: "danger", success: "false"}
+            return response;
+          }
+      },
+  
+      createCourt: function(court_data) {
+          if(tournament_service.misc.test_mode_active == 0)
+          {
+              return tournament_service.mockData.courts[0];
+          }
+          else
+          {   
+             tournament_service.loadMockData();
+              var last = tournament_service.mockData.courts.slice(-1)[0];
+              if( typeof last === 'undefined')
+                last = { id: 0};
+              court_data.id = last.id + 1;
+              tournament_service.mockData.courts.push(court_data);
+              tournament_service.saveMockData();
+              var response = { message: `Created court ${court_data.name}` , data: JSON.parse(JSON.stringify(court_data)), message_type: "success", success: "true"}
+              return response;
+          }
+      },
+  
+      updateCourt: function(court_data) {
+          if(tournament_service.misc.test_mode_active == 0)
+          {
+              return tournament_service.mockData.courts[0];
+          }
+          else
+          {  
+            tournament_service.loadMockData(); 
+              var last = tournament_service.mockData.courts.slice(-1)[0];
+              court_data.id = last.id + 1;
+              tournament_service.mockData.courts.push(court_data);
+              var response = { message: "Court updated" , data: JSON.parse(JSON.stringify(court_data)), message_type: "success", success: "true"}
+              tournament_service.saveMockData();
+              return response;
+          }
+      },
+  
+      removeCourt: function(id) {
+          if(tournament_service.misc.test_mode_active == 0)
+          {
+              return tournament_service.mockData.courts[0];
+          }
+          else
+          {   
+            tournament_service.loadMockData();
+            tournament_service.mockData.courts = tournament_service.mockData.courts.filter(court => court.id != id);
+            var response = { message: "Court removed" , data: {}, message_type: "success", success: "true"}
+            tournament_service.saveMockData();
+            return response;
+          }
+      },
 }
