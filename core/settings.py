156622558947756f0e6539f8c6b7c1077768baf1
Tim Kunz <tim.kunz@schmoll-maschinen.de>
2021-05-15 20:56:10 +0200

    settings.py STATICFILES_STORAGE

--- core/settings.py
+++ core/settings.py
@@ -134,6 +134,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'core/static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 #############################################################
 #############################################################
 
