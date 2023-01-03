485c27fd4edd0ef8b482a6c9c1ba71342e81c7c2
Tim Kunz <timkunz@gmx.net>
2023-01-03 21:30:10 +0000

    * commented celer

--- beachhandball_app/tasks.py
+++ beachhandball_app/tasks.py
@@ -1,12 +1,12 @@
-from celery import shared_task
+#from celery import shared_task
 from beachhandball_app import helper
 
-@shared_task
+#@shared_task
 def beach_debug_task():
     print('ENTER')
     print('Beach Request: ')
 
-@shared_task
+#@shared_task
 def update_user_tournament_events_async(gbo_user, tourn):
     print('ENTER')
     helper.update_user_tournament_events(gbo_user, tourn)
